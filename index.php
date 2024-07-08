b1a8dc20a3c5349b18239563648cdd0cd2d3aa15
ghofrane <lamouchighofrane8@gmail.com>
2024-07-08 17:14:13 +0200

    add user

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+    
+    require_once('./db_connect/index.php');
+
+    $users=$pdo->prepare("select * from users");
+    $users->execute();
+
+    $res=$users->fetchAll();
+
+    $page_titel="List Users";
+    $template="index";
+
+    include "./layout.phtml";
+?>
