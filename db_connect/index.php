32455f69cce25d3183f6ec603ae77e497b49011d
ghofrane <lamouchighofrane8@gmail.com>
2024-07-22 17:21:40 +0200

    signup

--- db_connect/index.php
+++ db_connect/index.php
@@ -1,10 +1,7 @@
 <?php 
-
-   define('DB_HOST','localhost');
-   define('DB_USER','root');
-   define('DB_Pass','');
-   define('DB_name','boycode');
-  
-   $pdo=new PDO('mysql:host='.DB_HOST.';dbname='.DB_name,DB_USER,DB_Pass);
-
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_Pass','');
+define('DB_name','boycott_bd');
+$pdo=new PDO('mysql:host='.DB_HOST.';dbname='.DB_name,DB_USER,DB_Pass);
 ?>
